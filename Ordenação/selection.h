3a6c7410fa4209bc9d7402a24fd9529230f4c24e
André Nascimento <37906395+AndreNasci@users.noreply.github.com>
2020-05-19 11:15:34 -0300

    Update selection.h

--- Ordenação/selection.h
+++ Ordenação/selection.h
@@ -3,22 +3,22 @@ using namespace std;
 
 // Metodo de ordenacao Selection Sort
 void selectionSort( string vetor[], int tam ) {
-  
-  string aux;
-  int min;
-  
-  for(int i=0; i<(tam-1); i++) {
-    min = i;
-  
+
+	string aux;
+	int min;
+
+	for(int i=0; i<(tam-1); i++) {
+		min = i;
+
 		// procura pelo menor elemento
-    for(int j = (i+1); j<tam; j++) {
-      if( vetor[j] < vetor[min] )
-        min = j;
-    }
-  
+		for(int j = (i+1); j<tam; j++) {
+			if( vetor[j] < vetor[min] )
+				min = j;
+		}
+
 		// move menor elemento para o incício do vetor
-    aux = vetor[i];
-    vetor[i] = vetor[min];
-    vetor[min] = aux;
-  }
+		aux = vetor[i];
+		vetor[i] = vetor[min];
+		vetor[min] = aux;
+	}
 }
